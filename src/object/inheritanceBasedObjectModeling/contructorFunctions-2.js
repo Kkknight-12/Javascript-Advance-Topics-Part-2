77cc5eab9b3948a396616f6d6e6d617fbcd357f7
Kkknight-12 <mayanks365@gmail.com>
2024-02-10 21:58:27 +0530

    Update Object notes

--- src/object/inheritanceBasedObjectModeling/contructorFunctions-2.js
+++ src/object/inheritanceBasedObjectModeling/contructorFunctions-2.js
@@ -1,31 +1,7 @@
 // Constructor functions
 /*
- * The constructor functions (aka object constructors pattern) have been the
- * modus operandi for building objects in JavaScript for many years. Although
- * object literals offer a terse way to define a single object, this method
- * doesn’t scale when you need to create hundreds of objects of the same
- * shape. In this case, the constructor function acts as a template to
- * initialize objects populated with different data. You’re probably familiar
- * with this pattern, but this section discusses some advanced techniques
- * that you may not have encountered before.
- *  */
-
-// Functions as templates
-/*
- * Using functions instead of straight object literals to build objects
- * allows your model to better evolve because you have much more control of
- * how the objects are built.
- *
- * Functions allow you to export a facade to the caller under which changes
- * don’t necessarily need to propagate to the calling code. The details of
- * how an object gets initialized, such as enforcing any preconditions, are
- * properly tucked away inside the constructor.
- *
- * The following code snippet, for example, never reveals unnecessary details
- * about the shape of HashTransaction or any operations that might take place
- * during instantiation. Encapsulation is always a good choice:
- *
- * const tx = new HashTransaction('luis@tjoj.com', 'luke@tjoj.com');
+ * Constructor functions in JavaScript are a way to create multiple objects
+ * with the same structure. They act as a blueprint for creating objects.
  * */
 
 function Transaction(sender, recipient) {
@@ -33,21 +9,37 @@ function Transaction(sender, recipient) {
   this.recipient = recipient
 }
 
+// You can create a new Transaction object like this:
+const tx11 = new Transaction('aaaa@tjoj.com', 'bbbb@tjoj.com')
+
+// Functions as templates
+/*
+ * HashTransaction is a specialized version of Transaction with an
+ * additional method calculateHash. It's created like this:
+ * */
+
 function HashTransaction(sender, recipient) {
   // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/new.target
   /*
-   * Detects whether the instantiation of the child object omits the new
-   * keyword and fixes the call.  */
+   * The new.target property lets you detect whether the constructor was
+   * called with new. If not, it creates a new instance correctly.
+   *  */
   if (!new.target) {
     return new HashTransaction(sender, recipient)
   }
 
   /*
-   * Calls the parent’s constructor to initialize any parent member
-   * properties into this object’s context
-   * */
+   * The Transaction.call(this, sender, recipient); line calls the
+   * Transaction constructor in the context of the new HashTransaction
+   * object, setting up the sender and recipient properties.
+   *  */
   Transaction.call(this, sender, recipient)
 
+  /*
+   * The calculateHash method is added directly to each HashTransaction object.
+   * This means each object gets its own copy of the method, which is not
+   * memory efficient if you're creating many objects.
+   *  */
   this.calculateHash = function calculateHash() {
     const data = [this.sender, this.recipient].join('')
     let hash = 0,
@@ -80,6 +72,9 @@ const tx2 = new HashTransaction('luis@tjoj.com', 'luke@tjoj.com')
 console.log(tx1.calculateHash === tx2.calculateHash) // false
 
 // ------------------------------------------------------------------------
+
+// Sharing properties by using constructors and prototypes
+
 /*
  * To fix this problem, you need to configure how prototypes links are set up
  * as new objects are created.
@@ -94,16 +89,22 @@ console.log(tx1.calculateHash === tx2.calculateHash) // false
  * */
 
 /*
- * This object is added to facilitate code sharing and reuse, especially
- * with methods, where it’s unnecessary to define more than one copy. Hence,
- * a more optimal approach is to add calculateHash to HashTransaction’s
- * prototype so that it’s shared among all HashTransaction instances, for
- * example:
+ * To solve this, you can add the calculateHash method to the
+ * HashTransaction prototype, so all HashTransaction objects share the
+ * same method:
  * */
-
 HashTransaction.prototype.calculateHash2 = function calculateHash() {
   // ...
 }
+
+/*
+ * Now, all HashTransaction objects share the same calculateHash method,
+ * which is more memory efficient. The prototype property is an object shared
+ * among all instances of a constructor function. When you try to access a
+ * property or method on an object, JavaScript will first look on the object
+ * itself, and if it doesn't find it there, it will look on the object's
+ * prototype. This is how JavaScript implements inheritance.
+ * */
 
 /*
  * With this slight twist, these two properties refer to the same memory
@@ -119,12 +120,25 @@ Transaction.prototype.displayTransaction = function displayTransaction() {
   return `Transaction from ${this.sender} to ${this.recipient}`
 }
 
-console.log(
-  'Transaction.prototype isPrototypeOf tx ',
-  Transaction.prototype.isPrototypeOf(tx),
-) // false")
+// console.log(tx1.displayTransaction())
+// tx1.displayTransaction is not a function
+/*
+ * The displayTransaction method is not available on the tx1 object. This is
+ * because the method is added to the Transaction prototype, not the
+ * HashTransaction prototype. To fix this, you need to link the
+ * HashTransaction prototype to the Transaction prototype:
+ * */
 
+// check
+console.log(
+  'is Transaction prototype of tx',
+  Transaction.prototype.isPrototypeOf(tx),
+) // false
+
+console.log('----------------------')
 // ------------------------------------------------------------------------
+
+// Linking prototypes with Object.create
 
 /*
  * You can fix this problem easily. As before, you can use Object.create. The
@@ -135,29 +149,20 @@ function TransactionC(sender, recipient) {
   this.sender = sender
   this.recipient = recipient
 }
+
 TransactionC.prototype.displayTransaction = function () {
   return `Transaction from ${this.sender} to ${this.recipient}`
 }
 
 function HashTransactionC(sender, recipient) {
+  // when the function is called without the new keyword
+  // it will return a new instance of HashTransactionC
+  // with the sender and recipient properties set
   if (!new.target) {
     return new HashTransactionC(sender, recipient)
   }
   TransactionC.call(this, sender, recipient)
 }
-
-// HashTransactionC.prototype.calculateHashC = function () {
-//   const data = [this.sender, this.recipient].join("");
-//   let hash = 0,
-//     i = 0;
-//   while (i < data.length) {
-//     hash = ((hash << 5) - hash + data.charCodeAt(i++)) << 0;
-//   }
-//   return hash ** 2;
-// };
-
-// const txCC = new HashTransactionC("abc.com", "zyx@tjoj.com");
-// console.log("txCC ", txCC.calculateHashC());
 
 /*
  * Links prototypes for the lookup mechanism to work in case you need to
@@ -172,6 +177,8 @@ HashTransactionC.prototype = Object.create(TransactionC.prototype)
  * */
 HashTransactionC.prototype.constructor = HashTransactionC
 
+// After setting up the prototype chain,
+// you can add the calculateHash method
 HashTransactionC.prototype.calculateHashC = function () {
   const data = [this.sender, this.recipient].join('')
   let hash = 0,
@@ -185,25 +192,42 @@ HashTransactionC.prototype.calculateHashC = function () {
 const txC = new HashTransactionC('luis@tjoj.com', 'luke@tjoj.com')
 const tx2C = new HashTransactionC('luis@tjoj.com', 'luke@tjoj.com')
 
-console.dir('txC ', txC)
+console.log('txC ', txC)
 
-// console.log(TransactionC.prototype.isPrototypeOf(txC)); // true
-// console.log("txC ", txC);
+// txC is an instance of HashTransactionC
+// HashTransactionC is a constructor function
+// and its prototype is linked to the TransactionC prototype
+
+console.log(
+  'TransactionC.prototype.isPrototypeOf(txC) ',
+  TransactionC.prototype.isPrototypeOf(txC),
+) // true
+
+console.log(
+  'HashTransactionC.prototype.isPrototypeOf(txC) ',
+  HashTransactionC.prototype.isPrototypeOf(txC),
+) // true
+// both TransactionC and HashTransactionC prototypes are in the prototype chain of txC
+
+// calling the displayTransaction method
+// which is added to the Transaction prototype
+// which is linked to the HashTransaction prototype.
+// Calling the displayTransaction method with the txC object
+// which is an instance of HashTransaction
 console.log('txC.calculateHashC ', txC.displayTransaction())
-// console.log(txC.displayTransaction === tx2C.displayTransaction); // true
 
-// console.log("tx; ", txC);
+// both txC and tx2C share the same displayTransaction method
+console.log(txC.displayTransaction === tx2C.displayTransaction) // true
+
+console.log('txC.__proto__; ', txC.__proto__)
 /*
- *  HashTransactionC {
- *     sender: 'luis@tjoj.com',
- *     recipient: 'luke@tjoj.com'
- * }
+TransactionC {
+  constructor: [Function: HashTransactionC],
+  calculateHashC: [Function (anonymous)]
+}
  */
 
-// console.log("tx.__proto__; ", txC.__proto__);
-//  TransactionC { constructor: [Function: HashTransactionC] }
-
-// console.log("tx.__proto__.__proto__; ", txC.__proto__.__proto__);
+console.log('tx.__proto__.__proto__; ', txC.__proto__.__proto__)
 //  { displayTransaction: [Function: displayTransaction] }
 
 // ------------------------------------------------------------------------
@@ -211,10 +235,7 @@ console.log('txC.calculateHashC ', txC.displayTransaction())
 // Difference between __proto__ and prototype
 
 /*
- * Reading the sample code, you have encountered references to two
- * properties: __proto__ and prototype. As I said earlier, __proto__ is
- * discouraged, but prototype isn’t.
- * In case you’re wondering what the difference is, __proto__is the object
+ * the difference is, __proto__is the object
  * used in the lookup chain to resolve methods, whereas prototype is the
  * object used to build __proto__when you create an object with new.
  *  */
